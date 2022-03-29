1f0e9b9cf4f92d23b66d7fbde878aac102641bfd
Kallista Rhoc <kallista1.rhoc@epitech.eu>
2022-03-29 17:04:10 +0200

    [MOD] return result from values closest to X and Y in csv file. Fixed result format

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-from operator import delitem
-import sys
 import csv
-import math
-import pyproj
 import json
-import numpy
 from flask import Flask, render_template, request
 import flask
 import requests
@@ -20,50 +15,48 @@ OPERATORS = {
     "20820": "Bouygue"
 }
 
-def lamber93_to_gps(x, y):
-	lambert = pyproj.Proj('+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')
-	wgs84 = pyproj.Proj('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
-	# x = 102980
-	# y = 6847973
-	long, lat = pyproj.transform(lambert, wgs84, x, y)
-	return long, lat
-
-app = flask.Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
-def find_nearest(array, value):
-    array = numpy.asarray(array)
-    idx = (numpy.abs(array - value)).argmin()
-    return array[idx]
+def get_result(csv_file, address_properties, closest, index):
+    result = {}
+    for row in csv_file:
+        if row[index] == closest:
+            result[OPERATORS[row[0]].lower()] = {
+                "address": address_properties["label"],
+                "2G": True if row[3] == "1" else False,
+                "3G": True if row[4] == "1" else False,
+                "4G": True if row[5] == "1" else False
+            }
+            break
+    return result
+
+
+def get_closest(csv_file, lambert_x, lambert_y):
+    x_list = [row[1] for row in csv_file]
+    y_list = [row[2] for row in csv_file]
+    closest_x = min(x_list, key=lambda x: abs(int(x)-lambert_x))
+    closest_y = min(y_list, key=lambda x: abs(int(x)-lambert_y))
+    return (closest_x, closest_y)
+
 
 @app.route('/search', methods=['POST'])
 def search():
-	input = request.form["address"].replace(' ', '+')
-	info = requests.get(API + input + "&limit=1").content.decode("utf-8")
-	address_properties = json.loads(info)["features"][0]["properties"]
-	gps_coordinates = json.loads(info)["features"][0]["geometry"]["coordinates"]
-	lambert_x = address_properties["x"]
-	lambert_y = address_properties["y"]
-	csv_file = csv.reader(open(CSV_FILE_PATH, "r"), delimiter=";")
-	result = {
-		"address": address_properties["label"],
-		"operateur":"",
-		"2G": False,
-		"3G": False,
-		"4G": False
-	}
-	for row in csv_file:
-		if row[1] != 'X' and int(row[1]) >= lambert_x:
-			result["operateur"] = OPERATORS[row[0]]
-			result["2G"] = row[3]
-			result["3G"] = row[4]
-			result["4G"] = row[5]
-			break
-	return(result)
+    csv_file = list(csv.reader(open(CSV_FILE_PATH, "r"), delimiter=";"))[1:-1]
+    input = request.form["address"].replace(' ', '+')
+    info = requests.get(API + input + "&limit=1").content.decode("utf-8")
+    address_properties = json.loads(info)["features"][0]["properties"]
+    lambert_x = address_properties["x"]
+    lambert_y = address_properties["y"]
+    x, y = get_closest(csv_file, lambert_x, lambert_y)
+    result = get_result(csv_file, address_properties, y, 2)
+    result.update(get_result(csv_file, address_properties, x, 1))
+    return(result)
 
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
 
 app.run()
